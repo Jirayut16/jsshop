d4d5eb3778c69fb04ea72d232dce6110b25e92ef
Jirayut16 <jirayut.who@gmail.com>
2025-04-14 15:52:12 +0700

    update config db

--- server/Config/db.js
+++ server/Config/db.js
@@ -2,13 +2,17 @@
 import mongoose from "mongoose";
 import "dotenv/config";
 
+let isConnected = false;
+
 const connectDB = async () => {
+  if (isConnected) return;
   try {
     await mongoose.connect(process.env.MONGO_URL, {
       connectTimeoutMS: 10000,
       serverSelectionTimeoutMS: 10000,
       bufferCommands: false,
     });
+    isConnected = true;
     console.log("MongoDB connected successfully");
   } catch (err) {
     console.error("MongoDB connection error:", err);
